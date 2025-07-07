eaed4f08b08f73cdccc53a6ac9846a77ace00d94
chisriftgames <chisrift51@gmail.com>
2025-07-07 20:04:02 +0100

    updated to check for verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -10,16 +11,29 @@ def main():
     if not args:
         print("no prompt supplied")
         sys.exit(1)
-    user_prompt = " ".join(args)
+    verbose = ""
+    if sys.argv[-1] == "--verbose":
+        user_prompt = " ".join(args[:-1])
+        verbose = f"User prompt: {user_prompt}"
+    else:
+        user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=user_prompt
+        contents=messages
     )
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+    
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(verbose)
+        print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+    
     print(f"Response: {response.text}")
     
 
